70358d50f76be791cb0b7eda55b3a35280325d96
Levente Krizsán <lewalevi@gmail.com>
2023-05-15 22:31:00 +0200

    fix(file): add more robust mp3 checking

--- src/util.rs
+++ src/util.rs
@@ -11,15 +11,21 @@ pub fn check_msg(result: SerenityResult<Message>) {
 pub fn get_sounds<P: AsRef<Path>>(path: P) -> Result<Vec<String>, std::io::Error> {
     let files = std::fs::read_dir(path)?
         .filter_map(|f| {
+            tracing::info!("{:?}", &f);
             let entry = f.unwrap();
             let file_name_os = entry.file_name();
             let file_name = file_name_os.to_string_lossy();
-            let (name, ext) = file_name.rsplit_once('.').unwrap();
-            if ext != "mp3" {
+            if file_name.ends_with(".mp3") {
+                let (name, _) = file_name.rsplit_once('.').unwrap();
+                Some(name.to_string())
+            } else {
+                None
+            }
+            /*if ext != "mp3" {
                 None
             } else {
                 Some(name.to_string())
-            }
+            }*/
         })
         .collect();
 
